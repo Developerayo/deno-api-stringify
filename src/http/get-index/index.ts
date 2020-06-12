62313d575ae633fbc0d98f06714450e69f5eb578
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-06-12 20:54:59 +0100

    create handler

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,3 +1,12 @@
 let body = {
-    message: "Welcome to Deno"
-  }
+  message: "Welcome to Deno",
+};
+
+export async function handler(req: object) {
+  return {
+    statusCode: 201,
+    headers: {
+      "content-type": "application/json; charset=utf8",
+    },
+  };
+}
