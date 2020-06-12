d89f3c66fcf9390865e5a7a0ed8b62ca31dd592f
Shodipo Ayomide <20538832+Developerayo@users.noreply.github.com>
2020-06-12 20:55:21 +0100

    setup cache-control

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -7,6 +7,7 @@ export async function handler(req: object) {
     statusCode: 201,
     headers: {
       "content-type": "application/json; charset=utf8",
+      "cache-control": "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
     },
   };
 }
